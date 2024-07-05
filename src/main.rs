597352a9e4294b278cc465fb9dc8a377b68c96a9
Yehor Dykhov <hyoustone@yahoo.com>
2024-07-05 16:48:59 +0300

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,101 @@
+extern crate core;
+
+use regex::{Captures, Regex};
 use serde_json::{Number, Value};
 use std::env;
 
-// use serde_bencode;
+enum BencodedType {
+    List,
+    Number,
+    String,
+    Unknown,
+}
 
-#[allow(dead_code)]
+impl From<&str> for BencodedType {
+    fn from(value: &str) -> Self {
+        let first_char = value.chars().next().unwrap();
+        match &first_char {
+            'i' => Self::Number,
+            'l' => Self::List,
+            _ => {
+                if let Ok(_s) = &first_char.to_string().parse::<usize>() {
+                    return  Self::String
+                }
+
+                Self::Unknown
+            },
+        }
+    }
+}
+
+fn number_parser(line: &str, start: usize) -> (Value, usize) {
+    let num_sep: Regex = Regex::new(r"i(\d+)e").unwrap();
+    let cap: Captures = num_sep.captures(&line[start..]).unwrap();
+    let num_len: usize = cap[1].len();
+    let num = cap[1].parse::<i64>().unwrap();
+
+    (Value::Number(Number::from(num)), start + num_len + 1)
+}
+
+fn string_parser(line: &str, start: usize) -> (Value, usize) {
+    let word_sep: Regex = Regex::new(r"(\d+):([a-zA-Z/:\-.]+)").unwrap();
+    let cap: Captures = word_sep.captures(&line[start..]).unwrap();
+    let size_len = cap[1].len();
+    let size = cap[1].parse::<usize>().unwrap();
+    let word: &str = &cap[2][..size];
+
+    (Value::String(word.to_string()), start + size_len + word.len())
+}
+
+fn list_parser(encoded_list: &str) -> (Vec<Value>, usize) {
+    let num_regx: Regex = Regex::new(r"\d+").unwrap();
+
+    let mut list: Vec<Value> = vec![];
+    let len = encoded_list.len();
+    let mut start: usize = 1;
+
+    while start < len {
+        let ch = &encoded_list[start..start + 1];
+
+        if num_regx.is_match(ch) {
+            let (v, end) = string_parser(encoded_list, start);
+            start = end + 1;
+            list.push(v);
+            continue;
+        } else if let "i" = ch {
+            let (v, end) = number_parser(encoded_list, start);
+            start = end + 1;
+            list.push(v);
+            continue;
+        } else if let "l" = ch {
+            let (_l, _end) = list_parser(&encoded_list[start..]);
+            start = _end + 1;
+
+            list.push(Value::Array(_l));
+            continue;
+        }
+
+        break;
+    }
+
+    (list, start + 1)
+}
+
 fn decode_bencoded_value(encoded_value: &str) -> Value {
-    // If encoded_value starts with a digit, it's a number
-    if encoded_value.chars().next().unwrap().is_digit(10) {
-        // Example: "5:hello" -> "hello"
-        let colon_index = encoded_value.find(':').unwrap();
-        let number_string = &encoded_value[..colon_index];
-        let number = number_string.parse::<i64>().unwrap();
-        let string = &encoded_value[colon_index + 1..colon_index + 1 + number as usize];
-        return Value::String(string.to_string());
-    } else if encoded_value.chars().next().unwrap().is_alphabetic()
-        && encoded_value.chars().last().unwrap().is_alphabetic()
-    {
-        let start = encoded_value.find('i').unwrap();
-        let end = encoded_value.find('e').unwrap();
-        let number_string = &encoded_value[(start + 1)..end];
-        let number = number_string.parse::<i64>().unwrap();
+    match BencodedType::from(encoded_value) {
+        BencodedType::List => Value::Array(list_parser(encoded_value).0),
+        BencodedType::Number => {
+            let start = encoded_value.find('i').unwrap();
+            let end = encoded_value.find('e').unwrap();
+            let number_string = &encoded_value[(start + 1)..end];
+            let number = number_string.parse::<i64>().unwrap();
 
-        return Value::Number(Number::from(number));
-    } else {
-        panic!("Unhandled encoded value: {}", encoded_value)
+            Value::Number(Number::from(number))
+        }
+        BencodedType::String => string_parser(encoded_value, 0).0,
+        BencodedType::Unknown => {
+            panic!("Unhandled encoded value: {}", encoded_value)
+        }
     }
 }
 
